0da5df51e9ee1b5629a6ffac5e3e73fa8e42d7b5
rodrigocrespo <rod_crespo@outlook.com>
2017-05-09 17:24:15 -0300

    adicionei limite no mapa

--- programa_funcao_main.py
+++ programa_funcao_main.py
@@ -7,7 +7,7 @@ pyg.init()
 
 white = (255,255,255)
 Display_largura = 1152
-Display_altura = 684
+Display_altura = 654
 
 screen = pyg.display.set_mode((Display_largura,Display_altura))
 pyg.display.set_caption("Random Race")
@@ -25,7 +25,7 @@ lead_y_change = 0
 d_angle = 20
 angle = 180
 d_vel = 2
-vel = 5
+vel = 3
 
 out = False
 while out != True:
@@ -46,12 +46,11 @@ while out != True:
     lead_x += vel * math.cos(math.pi * angle / 180.0)
     lead_y -= vel * math.sin(math.pi * angle / 180.0)
     if lead_x >= Display_largura or lead_x < 0 or lead_y >= Display_altura or lead_y < 0:
+        pass
+    if lead_x >= 1152 or lead_x < 0 or lead_y >= 654 or lead_y < 0:
         out = True
-
-
     screen.blit(background, (0,0, Display_altura, Display_largura))
     screen.blit(car,[lead_x, lead_y, 10, 10])
-    pyg.draw.rect(screen, white, [lead_x, lead_y, 10, 10])# mudar isso pro carrinho
     pyg.display.update()
     clock.tick(27)
 
