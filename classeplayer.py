5757d587df1f2b4aa80c5622ddaf07650ba7a6bc
KLiuyeh <kevinl@al.insper.edu.br>
2017-05-13 17:57:54 -0300

    teste

--- classeplayer.py
+++ classeplayer.py
@@ -5,6 +5,7 @@ import os
 
 #--------------------------------------------------Programa Principal-------------------------------------------------------------------------------
 pyg.init()
+# definicao das cores
 
 white = (255,255,255)
 black = (0,0,0)
@@ -12,6 +13,9 @@ red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
 yellow = (255,255,0)
+
+angle = math.pi
+variacao_angle =(math.pi/180)*10
 
 Display_largura = 1152
 Display_altura = 654
@@ -29,37 +33,67 @@ class Player(pyg.sprite.Sprite):
             self.image.set_colorkey(black) # deixa transparente as partes a mais do retancgulo
 
             self.rect = self.image.get_rect()
+            # onde muda a localizacao do carrinho
             self.rect.centerx = 300
-            self.rect.bottom = 300
-            self.speedx = 0
+            self.rect.bottom = 300  # parte inferior
+            self.speedx = 0    # velocidade
+            self.speedy = 0
+            self.angle = 180
+
+
+
+
 
 
         def update(self):
             self.speedx = 0
             self.speedy = 0
+            self.angle  = 180
+
             keystate = pyg.key.get_pressed()
             #if keystate[pyg.K_LEFT]:
             #if keystate[pyg.K_RIGHT]:
 
-
             if keystate[pyg.K_DOWN]:
                 self.speedy = 5
+                self.rect.y += self.speedy
+                self.rect.x += self.speedx
+
             if keystate[pyg.K_UP]:
                 self.speedy = -5
-            if keystate[pyg.K_LEFT]:
-                self
+                self.rect.y += self.speedy
+                self.rect.x += self.speedx
 
-            self.rect.y += self.speedy
+            if keystate[pyg.K_LEFT]:
+                self.speedx = -10
+                self.angle -= 45
+                self.rect.y += self.speedy*math.sin(math.pi*self.angle/180)
+                self.rect.x += self.speedx*math.cos(math.pi*self.angle/180)
+            if keystate[pyg.K_RIGHT]:
+                self.speedx= 10
+                self.angle += 45
+                self.rect.y += self.speedy*math.sin(math.pi*self.angle/180)
+                self.rect.x += self.speedx*math.cos(math.pi*self.angle/180)
+
+
+
+
+
+
             if self.rect.right > Display_largura:
                 self.rect.right = Display_largura
             if self.rect.left < 0:
                 self.rect.left = 0
+
             if self.rect.bottom > Display_altura:  # ERRRO
-                self.rect.right = 0
+                self.rect.bottom = 0
+
+
 
 
 class Oponente(pyg.sprite.Sprite):
         def __init__(self):
+
             pyg.sprite.Sprite.__init__(self)
             self.image = pyg.image.load("carro.jpeg").convert()
             self.image.set_colorkey(black) # deixa transparente as partes a mais do retancgulo
@@ -78,15 +112,18 @@ class Oponente(pyg.sprite.Sprite):
             #if keystate[pyg.K_RIGHT]:
 
 
-            if keystate[pyg.K_S]:
+            if keystate[pyg.K_s]:
                 self.speedy = 5
-            if keystate[pyg.K_W]:
+            if keystate[pyg.K_w]:
                 self.speedy = -5
-            if keystate[pyg.K_A]:
+            if keystate[pyg.K_a]:
+                self
+            if keystate[pyg.K_d]:
                 self
 
             self.rect.y += self.speedy
             if self.rect.right > Display_largura:
+
                 self.rect.right = Display_largura
             if self.rect.left < 0:
                 self.rect.left = 0
@@ -102,7 +139,6 @@ oponente_grupo.add(oponente)
 all_sprites.add(player)
 all_sprites.add(oponente)
 
-
 running = True
 while running:
     # keep loop runing at the right speed
@@ -115,13 +151,25 @@ while running:
     #update
     all_sprites.update()
     # check to see if a mob hit player
-    #hits = pygame.sprite.spritecollide(player,oponente,False):
-    #if hits
+    '''def collide (player, oponente):
+        if hits = pyg.sprite.spritecollide(player,pista,False)
+            if hits:
+                player.speedy = player.speedy * 1/4
+            speedy = speedy * 1/4
+
+
+        if oponente == grama:
+            hits2 = pyg.sprite.spritecollide(oponente,pista,False)
+            speedy = speedy * 1/4
+
+        if oponente == player:'''
+
 
 
     #screen.fill(blue)
         #Draw / render]
     screen.blit(background, (0,0,Display_altura,Display_largura))
+
     all_sprites.draw(screen)
 
 
--- programa_funcao_main.py
+++ programa_funcao_main.py
@@ -27,21 +27,17 @@ d_angle = 20
 angle = 180
 d_vel = 2
 vel = 3
-<<<<<<< HEAD
-=======
 
-
->>>>>>> origin/master
 out = False
 #global direction
-if direction == "right":
-    head = pyg.transform.rotate(car,180)
-if direction == "left":
-    head = pyg.transform.rotate(car,180)
-if direction == "up":
-    head = pyg.transform.rotate(car,180)
-if direction == "down":
-    head = pyg.transform.rotate(car,180)
+#if direction == "right":
+    #head = pyg.transform.rotate(car,180)
+#if direction == "left":
+    #head = pyg.transform.rotate(car,180)
+#if direction == "up":
+    #head = pyg.transform.rotate(car,180)
+#if direction == "down":
+    #head = pyg.transform.rotate(car,180)
 
 #-----------------------------Game LOOP------------------------------------------------------------
 while out != True:
@@ -49,9 +45,7 @@ while out != True:
         #global direction
         if event.type == pyg.QUIT:
             out = True
-<<<<<<< HEAD
-            
-=======
+
         if event.type == pyg.KEYDOWN:
 
                 if event.key == pyg.K_LEFT:
@@ -68,7 +62,7 @@ while out != True:
                 elif event.key == pyg.K_DOWN:
                     direction = "down"
                     vel -= d_vel
->>>>>>> 0da5df51e9ee1b5629a6ffac5e3e73fa8e42d7b5
+
 
 
     lead_x += vel * math.cos(math.pi * angle / 180.0)
@@ -78,12 +72,12 @@ while out != True:
     if lead_x >= 1152 or lead_x < 0 or lead_y >= 654 or lead_y < 0:
         out = True
     screen.blit(background, (0,0, Display_altura, Display_largura))
-<<<<<<< HEAD
+
     screen.blit(pyg.transform.rotate(car,angle),[lead_x, lead_y, 10, 10])
-=======
+
     screen.blit(carro,[lead_x,lead_y,10,10])
 
->>>>>>> origin/master
+
     pyg.display.update()
     clock.tick(27)
 
