e7a1c044b7007153db00970af4f9889a16b00940
Gabriel Salvador <gabrielps2@al.insper.edu.br>
2017-05-16 17:54:22 -0300

    continuar a partir daqui

--- Main.py
+++ Main.py
@@ -3,8 +3,13 @@ from pygame.locals import *
 import time
 import math
 import os
+from classes import *
+#-------------------Class-------------------------------------------------------
 
+
+#-------------------------------------------------------------------------------
 pyg.init()
+
 
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder,"resourses\imagens")
@@ -16,6 +21,13 @@ screen = pyg.display.set_mode((500,500))
 #background = pyg.image.load(os.path.join(img_folder,"FundoDemo.png")).convert()
 pista = pyg.image.load(os.path.join(img_folder,"track final.png"))
 carro = pyg.image.load(os.path.join(img_folder,"carro vermelho Demo.png"))#.convert_alpha()
+#carro_mask = pyg.mask.from_surface((os.path.join(img_folder,"carro vermelho mask.png")))
+#carro_mask = carro_mask(15,24)
+#pista_mask = pista_mask(1383,1448.4)
+#bg_mask = bg_mask(1500,1500)
+#pista_mask = pyg.image.load(os.path.join(img_folder,"track final pista mask black.png"))
+bg_mask = pyg.image.load(os.path.join(img_folder,"track final mask white.png"))
+#carro_mask = pyg.image.load(os.path.join(img_folder,"carro vermelho mask.png"))
 pistax= 0
 pistay= -1000
 xpos = 275
@@ -23,13 +35,30 @@ ypos = 350
 keys=[False,False,False,False]
 direction = 0
 forward = 0
+WHITE = (255, 255, 255, 255)
+#lista de sprites:
+#all_sprites_carros = pyg.sprite.Group()#sprites de carros
+#all_sprites_extra = pyg.sprite.Group()#sprites da pista e BG
+#adiciona ao grupo de sprites:
+#all_sprites_carros.add(carro_mask)
+#all_sprites_extra.add(pista_mask)
+#all_sprites_extra.add(bg_mask)
 
 
-running = 1
+running = True
 while running:
     pyg.display.set_caption('Random Race')
     screen.fill(0)
+    # Verifica se o carro bateu na pista.
+    print(bg_mask.get_at((int(xpos - pistax), int(ypos - pistay))))
+    if bg_mask.get_at((int(xpos - pistax), int(ypos - pistay))) == WHITE:
+        pyg.quit()
+        exit(0)
 
+    # lista_de_colisao = pyg.sprite.spritecollide(all_sprites_carros,all_sprites_extra,False)
+    # for carro in lista_de_colisao:
+    #     print("carro bateu")
+    #     running = False
     if keys[0]==True:
         direction+= 2
     if keys[1]==True:
@@ -48,10 +77,13 @@ while running:
     #screen.blit(background,(0,0))
     screen.blit(pista, (pistax,pistay))
     screen.blit(carro_rot, (xpos,ypos))
+    #screen.blit(carro_mask, (xpos,ypos))
     pyg.display.flip()
     time.sleep(0.02)
 
     for event in pyg.event.get():
+        #da update no grupo de sprites:
+        # all_sprites_list.update()
     # checa se sai do jogo
         if event.type==pyg.QUIT:
             # SAida do jogo
--- README.txt
+++ README.txt
@@ -7,6 +7,12 @@ dimensoes para construçoes da pista:
 
 TODOS DEVEM SER SALVOS EM MODO INDEX E NAO RGB!
 
+Cores das Mascaras:
+ BLUE = (0,30,255) ------> Carros
+ WHITE = (255,255,255) -----> BG
+ BLACK = (0,0,0) -----> Pista
+ YEllOW = (246,255,0) -------> faixa de contagem 
+
 Trabalhar somente o arquivo "Main.py"
 
 NAO mecher no codigo de LOOP principal! 
