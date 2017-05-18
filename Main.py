7c831feaf74481f7b61abb822be2d23791b571a0
rodrigocrespo <rod_crespo@outlook.com>
2017-05-18 15:27:20 -0300

    jogo com limites e a velocidade um pouco arrumada

--- Main.py
+++ Main.py
@@ -25,7 +25,7 @@ carro = pyg.image.load(os.path.join(img_folder,"carro vermelho Demo.png"))#.conv
 #carro_mask = carro_mask(15,24)
 #pista_mask = pista_mask(1383,1448.4)
 #bg_mask = bg_mask(1500,1500)
-#pista_mask = pyg.image.load(os.path.join(img_folder,"track final pista mask black.png"))
+#pista_mask = pyg.image.load(os.path.join(img_folder,"track final pista mask black.png")).convert_alpha()
 bg_mask = pyg.image.load(os.path.join(img_folder,"track final mask white.png"))
 #carro_mask = pyg.image.load(os.path.join(img_folder,"carro vermelho mask.png"))
 pistax= 0
@@ -36,6 +36,8 @@ keys=[False,False,False,False]
 direction = 0
 forward = 0
 WHITE = (255, 255, 255, 255)
+BLACK = (255, 255, 255, 0)
+#BLACK =()
 #lista de sprites:
 #all_sprites_carros = pyg.sprite.Group()#sprites de carros
 #all_sprites_extra = pyg.sprite.Group()#sprites da pista e BG
@@ -50,24 +52,46 @@ while running:
     pyg.display.set_caption('Random Race')
     screen.fill(0)
     # Verifica se o carro bateu na pista.
-    print(bg_mask.get_at((int(xpos - pistax), int(ypos - pistay))))
+    
+#    print(bg_mask.get_at((int(xpos - pistax), int(ypos - pistay))))
+    print(pistax,pistay)
+    if pistax >= 270: 
+        pistax = pistax -5 
+        forward = 0.01
+    if pistax <= -1200:
+        pistax = pistax + 5
+        forward = 0.01
+    if pistay >= 350:
+        pistay= pistay - 5
+        forward = 0.01
+    if pistay <= -1125:
+        pistay = pistay + 5
+        forward = 0.01
+    
     if bg_mask.get_at((int(xpos - pistax), int(ypos - pistay))) == WHITE:
-        pyg.quit()
-        exit(0)
+        if forward >2:
+            forward = 2
+        if keys[2]==True:
+            forward = 0
+            forward = -2
+        if keys[3]==True:
+            forward = 0
+            forward = 2
 
     # lista_de_colisao = pyg.sprite.spritecollide(all_sprites_carros,all_sprites_extra,False)
     # for carro in lista_de_colisao:
     #     print("carro bateu")
-    #     running = False
+    
+#     running = False
+    #if bg_mask.get_at((int(xpos - pistax), int(ypos - pistay))) == NO_COLOR:
+    if keys[2]==True:
+        forward-= 0.08
+    if keys[3]==True and forward <= 0:
+        forward+= 0.08
     if keys[0]==True:
         direction+= 2
     if keys[1]==True:
         direction-= 2
-    if keys[2]==True:
-        forward-= 0.2
-    if keys[3]==True and forward <= 0:
-        forward+= 0.2
-
     movex=math.cos(direction/57.29)*forward
     movey=math.sin(direction/57.29)*forward
     pistax+=movex
@@ -76,11 +100,18 @@ while running:
     carro_rot = pyg.transform.rotate(carro,direction)
     #screen.blit(background,(0,0))
     screen.blit(pista, (pistax,pistay))
+#    screen.blit(pista_mask, (pistax,pistay))
     screen.blit(carro_rot, (xpos,ypos))
     #screen.blit(carro_mask, (xpos,ypos))
     pyg.display.flip()
     time.sleep(0.02)
-
+    '''if pistax >= 280 or pistax <= -1200 and pistay >= 360 or pistay <= -1130:
+        forward  
+        xpos -= 5
+        ypos -= 5
+        pistay += movey
+        pistax -= movex
+'''
     for event in pyg.event.get():
         #da update no grupo de sprites:
         # all_sprites_list.update()
@@ -99,6 +130,9 @@ while running:
                 keys[2]=True
             elif event.key==K_DOWN:
                 keys[3]=True
+            elif event.key == pyg.K_ESCAPE:
+                pyg.quit()
+                exit(0)
 
         if event.type == pyg.KEYUP:
             if event.key==pyg.K_LEFT:
@@ -106,6 +140,7 @@ while running:
             elif event.key==pyg.K_RIGHT:
                 keys[1]=False
             elif event.key==pyg.K_UP:
+                forward = -2
                 keys[2]=False
             elif event.key==pyg.K_DOWN:
                 keys[3]=False
